84c9fad3e9f3ecbc0a786e15c37546801a822151
amin <ihelpmotivate@gmail.com>
2021-02-13 15:21:48 -0500

    edited array key-value and got the Get Random function working
    I reversed the key and values to make the author the key of the array and the quote the value
    
    I got the get random function to generate a random element from the array

--- inc/functions.php
+++ inc/functions.php
@@ -1,29 +1,34 @@
 <?php
 // --- PHP - Random Quote Generator ---
 
-// Create the Multidimensional array of quote elements and name it quotes
+
 
 
 $quotes = array(
-// Each inner array element should be an associative array 
-  array('"The only way to learn a new programming language is by writing programs in it."' => 'Dennis   Richie'),
-  array('"In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.'=> 'Andrew Hunt'),
-  array('"Testing leads to failure, and failure leads to understanding." ' => 'Burt Rutan'),
-  array('"The best error message is the one that never shows up." '=> 'Thomas Fuchs'),
-  array('“Programs must be written for people to read, and only incidentally for machines to execute.”' => ' Harold Abelson')
+
+  array('Dennis   Richie' => '"The only way to learn a new programming language is by writing programs in it."'),
+  array('Andrew Hunt'=> '"In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.'),
+  array('Burt Rutan' => '"Testing leads to failure, and failure leads to understanding."'),
+  array('Thomas Fuchs'=> '"The best error message is the one that never shows up."'),
+  array('Harold Abelson' => '“Programs must be written for people to read, and only incidentally for machines to execute.”')
  
 );
 
-var_dump($quotes);
+
   
-// Create the getRandomQuuote function and name it getRandomQuote
+
 function getRandomQuote(){
+
+  global $quotes;
+  $randomElement;
+
+  $randomElement = $quotes[rand(0,4)];
+  var_dump($randomElement);
 
 } 
 
+getRandomQuote();
 
-
-// Create the printQuote funtion and name it printQuote
 function printQuote(){
   
 }
