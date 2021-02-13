88ea239de722dd60f7e868c4648394fd5b48f6d8
amin <ihelpmotivate@gmail.com>
2021-02-12 22:43:36 -0500

    functions skeleton
    got the skeleton setup for the functions.php page

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,30 @@
-// PHP - Random Quote Generator
+<?php
+// --- PHP - Random Quote Generator ---
 
 // Create the Multidimensional array of quote elements and name it quotes
-// Each inner array element should be an associative array
 
 
+$quotes = array(
+  // Each inner array element should be an associative array
+  array(),
+  array()
+  
+);
 
+  
 // Create the getRandomQuuote function and name it getRandomQuote
+function getRandomQuote(){
+
+} 
 
 
 
 // Create the printQuote funtion and name it printQuote
+function printQuote(){
+  
+}
+
+
+
+
+?>
