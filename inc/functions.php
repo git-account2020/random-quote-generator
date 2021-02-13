dda9b7b4af603eddb1c8c836010dd0fa0db82280
amin <ihelpmotivate@gmail.com>
2021-02-13 17:13:14 -0500

    getRandomQuote function working based on requirements

--- inc/functions.php
+++ inc/functions.php
@@ -3,42 +3,44 @@
 
 
 
-
 $quotes = array(
-  // Each inner array element should be an associative array 
-    array('quote' => '"The only way to learn a new programming language is by writing programs in it."',
-          'source' => 'Dennis   Richie'),
-    array('quote' => '"In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.',
-          'source' => 'Andrew Hunt'),
-     array('quote' => '"Testing leads to failure, and failure leads to understanding."',
-          'source' => 'Burt Rutan'),
-     array('quote' => '"The best error message is the one that never shows up."',
-          'source' => 'Thomas Fuchs'),
-     array('quote' => '“Programs must be written for people to read, and only incidentally for machines to execute.”',
-          'source' => 'Harold Abelson')
-  );
-  
+
+  array('quote' => '"The only way to learn a new programming language is by writing programs in it."',
+        'source' => 'Dennis   Richie'),
+  array('quote' => '"In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.',
+        'source' => 'Andrew Hunt'),
+   array('quote' => '"Testing leads to failure, and failure leads to understanding."',
+        'source' => 'Burt Rutan'),
+   array('quote' => '"The best error message is the one that never shows up."',
+        'source' => 'Thomas Fuchs'),
+   array('quote' => '“Programs must be written for people to read, and only incidentally for machines to execute.”',
+        'source' => 'Harold Abelson')
+);
 
 
   
 
-function getRandomQuote(){
-
-  global $quotes;
+function getRandomQuote($array){
+  
+ 
   $randomElement;
 
-  $randomElement = $quotes[rand(0,4)];
+  $randomElement = $array[rand(0,4)];
   var_dump($randomElement);
+ 
+  return $randomElement;  
+
 
 } 
 
-getRandomQuote();
 
-function printQuote(){
+function printQuote($quotes){
+$quoteElement = getRandomQuote($quotes);
+
   
 }
 
-
+printQuote($quotes);
 
 
 ?>
