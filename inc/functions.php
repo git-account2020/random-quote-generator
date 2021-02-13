aae182a8d12268d8b2a4407a478a8aa49c6e577c
amin <ihelpmotivate@gmail.com>
2021-02-12 23:36:32 -0500

    created 5 array elements with quotes and names

--- inc/functions.php
+++ inc/functions.php
@@ -5,10 +5,13 @@
 
 
 $quotes = array(
-  // Each inner array element should be an associative array
+// Each inner array element should be an associative array 
   array('"The only way to learn a new programming language is by writing programs in it."' => 'Dennis   Richie'),
-  array(1,2)
-  
+  array('"In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.'=> 'Andrew Hunt'),
+  array('"Testing leads to failure, and failure leads to understanding." ' => 'Burt Rutan'),
+  array('"The best error message is the one that never shows up." '=> 'Thomas Fuchs'),
+  array('“Programs must be written for people to read, and only incidentally for machines to execute.”' => ' Harold Abelson')
+ 
 );
 
 var_dump($quotes);
