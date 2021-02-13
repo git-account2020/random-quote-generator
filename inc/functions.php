d8fd99a09ec153d389a54059ed8f3508f0fe374c
amin <ihelpmotivate@gmail.com>
2021-02-13 16:05:03 -0500

    updated quotes array to match guidelines

--- inc/functions.php
+++ inc/functions.php
@@ -5,14 +5,19 @@
 
 
 $quotes = array(
-
-  array('Dennis   Richie' => '"The only way to learn a new programming language is by writing programs in it."'),
-  array('Andrew Hunt'=> '"In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.'),
-  array('Burt Rutan' => '"Testing leads to failure, and failure leads to understanding."'),
-  array('Thomas Fuchs'=> '"The best error message is the one that never shows up."'),
-  array('Harold Abelson' => '“Programs must be written for people to read, and only incidentally for machines to execute.”')
- 
-);
+  // Each inner array element should be an associative array 
+    array('quote' => '"The only way to learn a new programming language is by writing programs in it."',
+          'source' => 'Dennis   Richie'),
+    array('quote' => '"In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.',
+          'source' => 'Andrew Hunt'),
+     array('quote' => '"Testing leads to failure, and failure leads to understanding."',
+          'source' => 'Burt Rutan'),
+     array('quote' => '"The best error message is the one that never shows up."',
+          'source' => 'Thomas Fuchs'),
+     array('quote' => '“Programs must be written for people to read, and only incidentally for machines to execute.”',
+          'source' => 'Harold Abelson')
+  );
+  
 
 
   
