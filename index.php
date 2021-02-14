97750c46c7b0789dbec61e3a7f019b5263646fbe
amin <ihelpmotivate@gmail.com>
2021-02-14 11:58:52 -0500

    functions removed extra quotations in array for quotes

--- index.php
+++ index.php
@@ -11,10 +11,6 @@
   <div class="container">
     <div id="quote-box">
       <?php include ('inc/functions.php') ?> 
-      
-      <!-- <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-      <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
-      -->
     </div>
     <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
   </div>
